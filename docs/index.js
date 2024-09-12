cc5aaefa3a8e1bbebc5b167c60d5625905337688
MohammadSoleimanikia <121093560+MohammadSoleimanikia@users.noreply.github.com>
2024-09-12 10:43:03 +0330

    add functionality that each key press have a animated effect and a sound

--- docs/index.js
+++ docs/index.js
@@ -1,5 +1,6 @@
 var buttonColors = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
+var userClickedPattern = [];
 
 function nextSequence() {
   // choose between 0 to 3
@@ -8,4 +9,34 @@ function nextSequence() {
   var randomChosenColor = buttonColors[randomNumber];
   //add the random color to the pattern
   gamePattern.push(randomChosenColor);
+  //   add flash animation to selected button
+  $("#" + randomChosenColor)
+    .fadeOut(300)
+    .fadeIn(300);
+  // play selected button audio
+  playSound(randomChosenColor);
+}
+
+//add click event listener to buttons
+$(".btn").on("click", (event) => {
+  // current target is the element that clicked.
+  var userChosenColor = $(event.currentTarget).attr("id");
+  userClickedPattern.push(userChosenColor);
+  playSound(userChosenColor);
+  animatePress(userChosenColor);
+});
+
+//play corresponding sound with name
+function playSound(name) {
+  var audio = new Audio("./assets/sounds/" + name + ".mp3");
+  audio.play();
+}
+
+// animate the pressed button
+function animatePress(currentColor) {
+  $("." + currentColor).addClass("pressed");
+  //remove class with a 100 ms delay
+  setTimeout(() => {
+    $("." + currentColor).removeClass("pressed");
+  }, 100);
 }
